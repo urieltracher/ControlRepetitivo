24b4f4066e9f97f3c96aeb33845d7c79131d1120
ウリエル・スラッシュ <126188671+urieltracher@users.noreply.github.com>
2023-05-09 02:27:21 -0600

    Add files via upload

---
+++ ControlRepetitivo/src/main/java/com/mycompany/controlrepetitivo/ControlRepetitivo.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.controlrepetitivo;
+
+/**
+ *
+ * @author 
+ */
+public class ControlRepetitivo {
+
+    public static void main(String[] args) {
+        
+        int numero = 1;
+        
+        while (numero <= 10) {
+            System.out.println(numero);
+            numero++;
+        }
+        
+    }
+}
+
